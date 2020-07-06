1817687ee1319c66e2f8cb4da385febca90c0c2d
luiz <luizfernandoklein@live.com>
2020-07-05 21:39:52 -0300

    chore: Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <h1>Landing page</h1>
-  );
-}
+const App: React.FC = () => <h1>Landing page</h1>;
 
 export default App;
